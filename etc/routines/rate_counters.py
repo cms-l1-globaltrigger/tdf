2be0249e3fe9bf2447d1c0d38f7f481658e9fe8c
Bernhard Arnold <Bernhard.Arnold@cern.ch>
2016-03-04 14:35:53 +0000

    refs trac-1350: added rates after prescales http://cms-project-l1osw-trac-backup.web.cern.ch/cms-project-l1osw-trac-backup/tickets/1350.html

--- etc/routines/rate_counters.py
+++ etc/routines/rate_counters.py
@@ -13,12 +13,12 @@ parser.add_argument('--limit', type=int, default=MAX_ALGORITHMS, help="limit num
 args = parser.parse_args(TDF_ARGS)
 
 rateCntBeforePrescaler = blockread(args.device, "gt_mp7_gtlfdl.rate_cnt_before_prescaler")
-#rateCntAfterPrescaler = read(args.device, "gt_mp7_gtlfdl.rate_cnt_after_prescaler")
+rateCntAfterPrescaler = blockread(args.device, "gt_mp7_gtlfdl.rate_cnt_after_prescaler")
 prescaleFactor = blockread(args.device, "gt_mp7_gtlfdl.prescale_factor")
 masks = blockread(args.device, "gt_mp7_gtlfdl.masks")
 
-print "| Algo | Rate before prescales | Rate before prescales (Hz) | Prescale factor | FINOR mask | Veto |"
+print "| Algo | Rate before prescales (Hz) | Rate after prescales (Hz) | Prescale factor | FINOR mask | Veto |"
 for i in range(min(MAX_ALGORITHMS, args.limit)):
-    print "| {0:4} | {1:21} | {2:26} | {3:15} | {4:10} | {5:4} |".format(
-        i, rateCntBeforePrescaler[i], int(rateCntBeforePrescaler[i] / LUMI_SEC), prescaleFactor[i], masks[i] & 0x1, masks[i] & 0x2
+    print "| {0:4} | {1:26} | {2:25} | {3:15} | {4:10} | {5:4} |".format(
+        i, int(rateCntBeforePrescaler[i] / LUMI_SEC), int(rateCntAfterPrescaler[i] / LUMI_SEC), prescaleFactor[i], masks[i] & 0x1, masks[i] & 0x2
     )
