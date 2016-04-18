bd025d8d43c311ce0eb911ec93d1342c63d2197a
Herbert Bergauer <Herbert.Bergauer@cern.ch>
2016-04-18 12:48:44 +0000

    refs trac-1350 : adjusted DEFAULT_INPUT_DELAY in tx_buffer_function_test_2_0_0 for frame v0.0.38 and updated gt_mp7_gtlfdl.xml for fdl v0.0.24 http://cms-project-l1osw-trac-backup.web.cern.ch/cms-project-l1osw-trac-backup/tickets/1350.html SVN Revision: 44640

--- etc/routines/tx_buffer_function_test_2_0_0.py
+++ etc/routines/tx_buffer_function_test_2_0_0.py
@@ -8,7 +8,9 @@ import os, re
 import time
 import datetime
 
-DEFAULT_INPUT_DELAY = 9
+## HB 2016-04-18: changed DEFAULT_INPUT_DELAY with FW frame v0.0.38, because of additional delay for bcres_d in dm.vhd.
+#DEFAULT_INPUT_DELAY = 9
+DEFAULT_INPUT_DELAY = 8
 DEFAULT_GTL_LATENCY = 6
 DEFAULT_SIZE = 170
 DEFAULT_RX_LINKS = '0-15'
