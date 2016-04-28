f2fc384f4253be7115e75fa11e14a85584a36fb7
jwittman <jwittman@cern.ch>
2016-04-28 10:14:26 +0000

    refs trac-1350: added script to delete multiple files at once from the uSD card http://cms-project-l1osw-trac-backup.web.cern.ch/cms-project-l1osw-trac-backup/tickets/1350.html

---
+++ etc/routines/deletefw.py
@@ -0,0 +1,51 @@
+# delete GT firmware bitfiles from uSD card(s).
+
+from tdf.extern import argparse
+import tarfile
+import tempfile
+import shutil
+import re, os
+
+def scansd(device):
+    """Read stdout results from butler command (bitfiles located on uSD).
+    Returns list of filenames stored on uSD card.
+    """
+    import tempfile
+    # Create temporary file in memory.
+    tmp = tempfile.SpooledTemporaryFile()
+    # Capture stdout from butler (should contain list of bitfiles).
+    mp7butler("scansd", device, stdout = tmp)
+    # Set file pointer to begin of temporary file.
+    tmp.seek(0)
+    # Read content of temporary file.
+    result = tmp.read().strip().split("\n")
+    # Close the temporary file.
+    tmp.close()
+    # Return list of bitfiles.
+    return [filename.strip() for filename in result]
+
+parser = argparse.ArgumentParser()
+parser.add_argument('device', help = "device defined in connections file")
+parser.add_argument('file', nargs="+", help = "bitfiles to delete from the SD card")
+args = parser.parse_args(TDF_ARGS)
+
+device = args.device
+print
+print " => checking uSD card contents on device `{device}'...".format(**locals())
+
+# Read stdout results from butler command (bitfiles located on uSD).
+filenames = scansd(device)
+    
+for filename in args.file:
+
+
+    # If file with same name already exists, delete it:
+    print " => bit file `{filename}' found on the SD card, deleting...".format(**locals())
+    mp7butler("deletefw", device, filename)
+
+device = args.device
+print
+print " => checking uSD card contents on device `{device}'...".format(**locals())
+
+# Read stdout results from butler command (bitfiles located on uSD).
+filenames = scansd(device)
