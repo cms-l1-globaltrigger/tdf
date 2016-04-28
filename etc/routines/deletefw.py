de377b985143c4fb408b78ac76d109f2ab75cd27
Bernhard Arnold <Bernhard.Arnold@cern.ch>
2016-04-28 10:50:17 +0000

    refs trac-1350: permitting capturing stderr from mp7butler; updated scansd() in deletefw routine http://cms-project-l1osw-trac-backup.web.cern.ch/cms-project-l1osw-trac-backup/tickets/1350.html

--- etc/routines/deletefw.py
+++ etc/routines/deletefw.py
@@ -10,42 +10,39 @@ def scansd(device):
     """Read stdout results from butler command (bitfiles located on uSD).
     Returns list of filenames stored on uSD card.
     """
-    import tempfile
     # Create temporary file in memory.
     tmp = tempfile.SpooledTemporaryFile()
     # Capture stdout from butler (should contain list of bitfiles).
-    mp7butler("scansd", device, stdout = tmp)
+    mp7butler("scansd", device, stderr = tmp)
     # Set file pointer to begin of temporary file.
     tmp.seek(0)
     # Read content of temporary file.
-    result = tmp.read().strip().split("\n")
+    filenames = []
+    for line in tmp.readlines():
+        result = re.search(r"([\w_\.]+\.bit)", line.strip())
+        if result:
+            filenames.append(result.group(1))
     # Close the temporary file.
     tmp.close()
     # Return list of bitfiles.
-    return [filename.strip() for filename in result]
+    return filenames
 
 parser = argparse.ArgumentParser()
 parser.add_argument('device', help = "device defined in connections file")
 parser.add_argument('file', nargs="+", help = "bitfiles to delete from the SD card. double check the filename, because the script will crash on missing files.")
 args = parser.parse_args(TDF_ARGS)
 
-device = args.device
 print
-print " => checking uSD card contents on device `{device}'...".format(**locals())
+print " => checking uSD card contents on device `{args.device}'...".format(**locals())
 
 # Read stdout results from butler command (bitfiles located on uSD).
-filenames = scansd(device)
-    
+filenames = scansd(args.device)
+
 for filename in args.file:
 
-
-    # If file with same name already exists, delete it:
-    print " => bit file `{filename}' found on the SD card, deleting...".format(**locals())
-    mp7butler("deletefw", device, filename)
-
-device = args.device
-print
-print " => checking uSD card contents on device `{device}'...".format(**locals())
-
-# Read stdout results from butler command (bitfiles located on uSD).
-filenames = scansd(device)
+    if filename in filenames:
+        # If file with same name already exists, delete it:
+        print " => bit file `{filename}' found on the SD card, deleting...".format(**locals())
+        mp7butler("deletefw", args.device, filename)
+    else:
+        print " ! ignoring non exising file `{filename}' ...".format(**locals())
