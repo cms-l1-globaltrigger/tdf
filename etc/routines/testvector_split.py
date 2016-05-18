873b3cbdc01364f11770121c70c3140966894930
Bernhard Arnold <Bernhard.Arnold@cern.ch>
2016-05-18 11:17:29 +0000

    refs trac-1350: updated testvector_split routine http://cms-project-l1osw-trac-backup.web.cern.ch/cms-project-l1osw-trac-backup/tickets/1350.html SVN Revision: 45485

--- etc/routines/testvector_split.py
+++ etc/routines/testvector_split.py
@@ -6,22 +6,6 @@ from tdf.core.xmlmenu import XmlMenu
 from tdf.extern import argparse
 import sys, os, copy
 
-def calc_mask(algorithms):
-    """Calculate algorithm mask using global indices."""
-    mask = 0
-    for algorithm in algorithms:
-        mask |= (1 << algorithm.index)
-    return mask
-
-def mask_algorithms(tv, mask):
-    """Masks algorithms using the global indices."""
-    for bx, algorithms in enumerate(tv._algorithms):
-        tv._algorithms[bx] = algorithms & mask
-
-def update_finor(tv):
-    """Update FinORs (after masking algorithms)."""
-    tv._finor = [bool(algorithms) for algorithms in tv._algorithms]
-
 parser = argparse.ArgumentParser(description="Split text vector into modules by information from XML file")
 parser.add_argument('testvector', help="test vector file")
 parser.add_argument('menu', help="XML menu file")
@@ -54,11 +38,9 @@ for module, algorithms in modules.iteritems():
     basename = os.path.splitext(os.path.basename(args.testvector))[0]
     filename = "{basename}_module_{module}.txt".format(**locals())
     module_tv = copy.deepcopy(tv)
+    TDF_INFO("update algorithms and FinOR...")
+    mask = [algorithm.index for algorithm in algorithms]
+    module_tv.maskAlgorithms(mask)
     with open(os.path.join(args.o, filename), 'w') as fp:
-        mask = calc_mask(algorithms)
-        TDF_INFO("update algorithm mask...", TDF.ALGORITHM.hexstr(mask))
-        mask_algorithms(module_tv, mask)
-        TDF_INFO("update FINOR using algorithm mask...")
-        update_finor(module_tv)
         TDF_INFO("writing", fp.name)
         fp.write(str(module_tv))
