1e86b7debba04f29dab8217bd28ccceb1751f463
jwittman <jwittman@cern.ch>
2016-06-16 10:24:47 +0000

    refs trac-1350: updated lumi section test script http://cms-project-l1osw-trac-backup.web.cern.ch/cms-project-l1osw-trac-backup/tickets/1350.html

--- etc/routines/lumi_counter_monitor.py
+++ etc/routines/lumi_counter_monitor.py
@@ -5,6 +5,7 @@ from tdf.extern import argparse
 from tdf.core import binutils
 from tdf.core import TDF
 import time
+import sys
 
 def read_lumi_counter(device):
     return read(device, "gt_mp7_frame.rb.tcm_status.luminosity_seg_nr")
@@ -24,20 +25,25 @@ args = parser.parse_args(TDF_ARGS)
 TDF_WARNING("note: hit CTRL + C to exit routine")
 
 previous_ls = read_lumi_counter(args.device)
-rate_cnt_before_prescaler = blockread(args.device, "gt_mp7_gtlfdl.rate_cnt_before_prescaler")
-err_det = read(args.device, "gt_mp7_frame.rb.tcm_status.err_det")
-print "| LumiSegNr | rate_before_prescale | algo | err_det |"
-print "| {0:>9} | {1:>20} | {2:>4} | {3:>7} |".format(previous_ls, rate_cnt_before_prescaler[args.index], args.index, err_det)
+#rate_cnt_before_prescaler = blockread(args.device, "gt_mp7_gtlfdl.rate_cnt_before_prescaler")
+#rate_cnt_after_prescaler = blockread(args.device, "gt_mp7_gtlfdl.rate_cnt_after_prescaler")
+#err_det = read(args.device, "gt_mp7_frame.rb.tcm_status.err_det")
+#print "| LumiSegNr | rate_before_prescale | rate_after_prescale | algo | err_det |"
+#print "| {0:>9} | {1:>20} | {2:>20} | {3:>4} | {4:>7} |".format(previous_ls, rate_cnt_before_prescaler[args.index], rate_cnt_after_prescaler[args.index], args.index, err_det)
 
 try:
     while True:
         current_ls = read_lumi_counter(args.device)
         if previous_ls != current_ls:
             rate_cnt_before_prescaler = blockread(args.device, "gt_mp7_gtlfdl.rate_cnt_before_prescaler")
+            rate_cnt_after_prescaler = blockread(args.device, "gt_mp7_gtlfdl.rate_cnt_after_prescaler")
             err_det = read(args.device, "gt_mp7_frame.rb.tcm_status.err_det")
-	    print "| LumiSegNr | rate_before_prescale | algo | err_det |"
-	    print "| {0:>9} | {1:>20} | {2:>4} | {3:>7} |".format(current_ls, rate_cnt_before_prescaler[args.index], args.index, err_det)
-            previous_ls = current_ls
+            print "| LumiSegNr | rate_before_prescale | rate_after_prescale  | algo | err_det |"
+            print "| {0:>9} | {1:>20} | {2:>20} | {3:>4} | {4:>7} |".format(previous_ls, rate_cnt_before_prescaler[args.index], rate_cnt_after_prescaler[args.index], args.index, err_det)
+            if rate_cnt_before_prescaler[args.index] != rate_cnt_after_prescaler[args.index]:
+                print "counter values not equal!"
+                sys.exit(0)
+        previous_ls = current_ls
         # throttle monitoring rate
         time.sleep(args.i)
 except KeyboardInterrupt:
