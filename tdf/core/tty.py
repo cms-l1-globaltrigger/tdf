862ef1fe1301f5c1c5c4d9f2215dad53b7452239
Bernhard Arnold <Bernhard.Arnold@cern.ch>
2017-09-26 14:35:13 +0000

    refs trac-1350: updated TTY control sequences http://cms-project-l1osw-trac-backup.web.cern.ch/cms-project-l1osw-trac-backup/tickets/1350.html

--- tdf/core/tty.py
+++ tdf/core/tty.py
@@ -12,6 +12,8 @@
 """
 TTY colors and control sequences.
 
+
+
 Example:
 
 >>> from tdf.core import tty
@@ -22,10 +24,11 @@ Example:
 
 import sys
 
-class TTYCtrlSeq(object):
-    """TTY control sequence."""
+class CSICode(object):
+    """CSI code for TTY."""
 
-    TTYCtrlSeqTemplate = '\033[{0}m'
+    PrivateMode = '\033['
+    TrailingIntermediate = ''
 
     def __init__(self, sequence, description=None, stream=sys.stdout):
         self.sequence = sequence if isinstance(sequence, list) else [sequence]
@@ -33,50 +36,101 @@ class TTYCtrlSeq(object):
         self.stream = stream
 
     def __add__(self, other):
-        """Add two control sequences."""
-        assert isinstance(other, TTYCtrlSeq)
+        """Add two CSI codes."""
+        assert isinstance(other, CSICode)
         sequence = self.sequence + other.sequence
         description = "{0}, {1}".format(self.description, other.description)
-        return TTYCtrlSeq(sequence, description, self.stream)
+        return self.__class__(sequence, description, self.stream)
 
     def __str__(self):
         """Print control sequences only on TTYs."""
         if self.stream.isatty():
             sequence = [format(code) for code in self.sequence]
-            return self.TTYCtrlSeqTemplate.format(';'.join(sequence))
+            return '{0}{1}{2}'.format(self.PrivateMode, ';'.join(sequence), self.TrailingIntermediate)
         return ''
 
-# Modifiers
-Reset = TTYCtrlSeq(0, "reset")
-Bold = TTYCtrlSeq(1, "bold")
-Dark = TTYCtrlSeq(2, "dark")
-Italic = TTYCtrlSeq(3, "italic")
-Underline = TTYCtrlSeq(4, "underline")
-Invert = TTYCtrlSeq(4, "invert")
-Hidden = TTYCtrlSeq(8, "hidden")
-Strikethrough = TTYCtrlSeq(8, "strikethrough")
+class ANSICtrlCode(CSICode):
+    """ANSI control code."""
 
-# Forground colors
-Black = TTYCtrlSeq(30, "foreground black")
-Red = TTYCtrlSeq(31, "foreground red")
-Green = TTYCtrlSeq(32, "foreground green")
-Yellow = TTYCtrlSeq(33, "foreground yellow")
-Blue = TTYCtrlSeq(34, "foreground blue")
-Magenta = TTYCtrlSeq(35, "foreground magenta")
-Cyan = TTYCtrlSeq(36, "foreground cyan")
-White = TTYCtrlSeq(37, "foreground white")
-Gray = TTYCtrlSeq(38, "foreground gray")
+    def __init__(self, n, trailing, description=None, stream=sys.stdout):
+        super(ANSICtrlCode, self).__init__(n, description, stream)
+        self.TrailingIntermediate = trailing
 
-# backgroudn colors
-BackgroundBlack = TTYCtrlSeq(40, "background black")
-BackgroundRed = TTYCtrlSeq(41, "background red")
-BackgroundGreen = TTYCtrlSeq(42, "background green")
-BackgroundYellow = TTYCtrlSeq(43, "background yellow")
-BackgroundBlue = TTYCtrlSeq(44, "background blue")
-BackgroundMagenta = TTYCtrlSeq(45, "background magenta")
-BackgroundCyan = TTYCtrlSeq(46, "background cyan")
-BackgroundWhite = TTYCtrlSeq(47, "background white")
-BackgroundGray = TTYCtrlSeq(48, "background gray")
+class SGRParam(CSICode):
+    """SGR (Select Graphic Rendition) parameter."""
+
+    TrailingIntermediate = 'm'
+
+# ANSI control codes
+def CursorUp(n): return ANSICtrlCode(n, 'A', "CUU cursor up {0}".format(n))
+def CursorDown(n): return ANSICtrlCode(n, 'B', "CUD cursor down {0}".format(n))
+def CursorForward(n): return ANSICtrlCode(n, 'C', "CUF cursor forward {0}".format(n))
+def CursorBack(n): return ANSICtrlCode(n, 'D', "CUB cursor back {0}".format(n))
+def CursorNextLine(n): return ANSICtrlCode(n, 'E', "CNL cursor next line(s) {0}".format(n))
+def CursorPreviousLine(n): return ANSICtrlCode(n, 'F', "CPL cursor previous line(s) {0}".format(n))
+def CursorHorizontalAbsolute(n): return ANSICtrlCode(n, 'G', "CHA cursor to column {0}".format(n))
+
+# SGR modifiers
+Reset = SGRParam(0, "reset")
+Bold = SGRParam(1, "bold")
+Faint = SGRParam(2, "faint")
+Italic = SGRParam(3, "italic")
+Underline = SGRParam(4, "underline")
+BlinkSlow = SGRParam(5, "blink slow")
+BlinkFast = SGRParam(6, "blink fast")
+Inverse = SGRParam(7, "inverse")
+Conceal = SGRParam(8, "conceal")
+CrossedOut = SGRParam(9, "crossed out")
+
+# SGR forground colors
+Black = SGRParam(30, "foreground black")
+Red = SGRParam(31, "foreground red")
+Green = SGRParam(32, "foreground green")
+Yellow = SGRParam(33, "foreground yellow")
+Blue = SGRParam(34, "foreground blue")
+Magenta = SGRParam(35, "foreground magenta")
+Cyan = SGRParam(36, "foreground cyan")
+White = SGRParam(37, "foreground white")
+Extended = SGRParam(38, "foreground extended (rgb)")
+Default = SGRParam(39, "foreground default")
+
+def ColorCode(n): return Extended + SGRParam([5, n]) # 0...255
+def RGB(r, g, b): return Extended + SGRParam([2, r, g, b])
+
+# SGR background colors
+BackgroundBlack = SGRParam(40, "background black")
+BackgroundRed = SGRParam(41, "background red")
+BackgroundGreen = SGRParam(42, "background green")
+BackgroundYellow = SGRParam(43, "background yellow")
+BackgroundBlue = SGRParam(44, "background blue")
+BackgroundMagenta = SGRParam(45, "background magenta")
+BackgroundCyan = SGRParam(46, "background cyan")
+BackgroundWhite = SGRParam(47, "background white")
+BackgroundExtended = SGRParam(48, "background extended (rgb)")
+BackgroundDefault = SGRParam(49, "background default")
+
+def BackgroundColorCode(n): return BackgroundExtended + SGRParam([5, n]) # 0...255
+def BackgroundRGB(r, g, b): return BackgroundExtended + SGRParam([2, r, g, b])
+
+# aixterm forground colors (not in standard)
+HighBlack = SGRParam(90, "high intensity foreground black (aixterm)")
+HighRed = SGRParam(91, "high intensity foreground red (aixterm)")
+HighGreen = SGRParam(92, "high intensity foreground green (aixterm)")
+HighYellow = SGRParam(93, "high intensity foreground yellow (aixterm)")
+HighBlue = SGRParam(94, "high intensity foreground blue (aixterm)")
+HighMagenta = SGRParam(95, "high intensity foreground magenta (aixterm)")
+HighCyan = SGRParam(96, "high intensity foreground cyan (aixterm)")
+HighWhite = SGRParam(97, "high intensity foreground white (aixterm)")
+
+# aixterm ackground colors (not in standard)
+HighBackgroundBlack = SGRParam(100, "high intensity background black (aixterm)")
+HighBackgroundRed = SGRParam(101, "high intensity background red (aixterm)")
+HighBackgroundGreen = SGRParam(102, "high intensity background green (aixterm)")
+HighBackgroundYellow = SGRParam(103, "high intensity background yellow (aixterm)")
+HighBackgroundBlue = SGRParam(104, "high intensity background blue (aixterm)")
+HighBackgroundMagenta = SGRParam(105, "high intensity background magenta (aixterm)")
+HighBackgroundCyan = SGRParam(106, "high intensity background cyan (aixterm)")
+HighBackgroundWhite = SGRParam(107, "high intensity background white (aixterm)")
 
 Colors = (
     Black,
@@ -87,7 +141,7 @@ Colors = (
     Magenta,
     Cyan,
     White,
-    Gray,
+    Default,
 )
 
 BackgroundColors = (
@@ -99,7 +153,29 @@ BackgroundColors = (
     BackgroundMagenta,
     BackgroundCyan,
     BackgroundWhite,
-    BackgroundGray,
+    BackgroundDefault,
+)
+
+HighColors = (
+    HighBlack,
+    HighRed,
+    HighGreen,
+    HighYellow,
+    HighBlue,
+    HighMagenta,
+    HighCyan,
+    HighWhite,
+)
+
+HighBackgroundColors = (
+    HighBackgroundBlack,
+    HighBackgroundRed,
+    HighBackgroundGreen,
+    HighBackgroundYellow,
+    HighBackgroundBlue,
+    HighBackgroundMagenta,
+    HighBackgroundCyan,
+    HighBackgroundWhite,
 )
 
 if __name__ == '__main__':
@@ -111,3 +187,19 @@ if __name__ == '__main__':
         for background in BackgroundColors:
             style = color + background
             print style, style.description, Reset
+
+    for color in HighColors:
+            print color, color.description, Reset
+
+    for color in HighColors:
+        for background in HighBackgroundColors:
+            style = color + background
+            print style, style.description, Reset
+
+    print " ...bar", CursorHorizontalAbsolute(1), "foo"
+
+    for r in range(0, 256, 25):
+        for g in range(0, 256, 25):
+            for b in range(0, 256, 25):
+                print "{}{}{}".format(RGB(r, g, b), "X", Reset),
+            print
